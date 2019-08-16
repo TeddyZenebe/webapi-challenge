69bd2d8f4b0392662977b3f1cc0af03e9635721f
Your Name <tfikrie@yahoo.com>
2019-08-16 19:39:26 -0400

    index.js ,server.js and router.js were created and configured and also dependencies are instaled using npm

---
+++ router.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const projectDb = require('./data/helpers/projectModel');
+const actionDb = require('./data/helpers/actionModel');
+const router = express.Router();
